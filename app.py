f3a9bd6d80caa2c047f9f8da1f51a4376241340b
HICHAM CHATER <45130784+hichamDRI@users.noreply.github.com>
2021-08-28 16:19:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ model_name_ar_fr="Helsinki-NLP/opus-mt-ar-fr"
 model_ar_fr, tokenizer_ar_fr = download_model(model_name_ar_fr)
 
 st.markdown("<h1 style='text-align: center; color: red;'>Machine Translation using Transformers</h1>", unsafe_allow_html=True)
-st.title('Frensh to Arabic Translator')
 
-text = st.text_area("Enter Text :", value='',key="FrenshText")
+st.title('Frensh to Arabic Translator')
+textFR = st.text_area("Enter Text :", value='',key="FrenshText")
 
 if st.button('Translate', key="FrenshBtn"):
     if text == '':
         st.write('Please enter Frensh text for translation') 
     else: 
-        inputs = tokenizer_fr_ar.encode(text, return_tensors="pt")
+        inputs = tokenizer_fr_ar.encode(textFR, return_tensors="pt")
         outputs = model_fr_ar.generate(inputs, max_length=128, num_beams=4, early_stopping=True)
         out=tokenizer_fr_ar.decode(outputs[0])
         st.text_area('Translation Result :', str(out).strip('<pad>'))
@@ -31,13 +31,13 @@ else: pass
 
 
 st.title('Arabic to Frensh Translator')
-text = st.text_area("Enter Text:", value='',key="ArabicText")
+textAR = st.text_area("Enter Text:", value='',key="ArabicText")
 
 if st.button('Translate', key="ArabicBtn"):
     if text == '':
         st.write('Please enter Arabic text for translation') 
     else: 
-        inputs = tokenizer_ar_fr.encode(text, return_tensors="pt")
+        inputs = tokenizer_ar_fr.encode(textAR, return_tensors="pt")
         outputs = model_ar_fr.generate(inputs, max_length=128, num_beams=4, early_stopping=True)
         out=tokenizer_ar_fr.decode(outputs[0])
         st.text_area('Translation Result :', str(out).strip('<pad>'))
