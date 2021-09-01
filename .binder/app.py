4340417e2d89c4604484b193c4c88ccd71b390de
HICHAM CHATER <45130784+hichamDRI@users.noreply.github.com>
2021-09-01 19:21:53 +0100

    Update app.py

--- .binder/app.py
+++ .binder/app.py
@@ -43,4 +43,11 @@ if st.button('Translate', key="ArabicBtn"):
         out=tokenizer_ar_fr.decode(outputs[0])
         st.text_area('Translation Result :', str(out).strip('<pad>'), key="FrenshResult")
 else: pass
+
+option = st.selectbox(
+     'Choose your option of translation',
+     ('Frensh To Arabic', 'Arabic To Frensh'))
+
+st.write('You selected:', option)
+
 st.markdown("<h3 style='text-align: center; color: green;'>Created By: CHATER hicham & ABOURACHID Tarik</h3>", unsafe_allow_html=True)
