b39c1c4af6ce928a29521162b13392eef2de655e
HICHAM CHATER <45130784+hichamDRI@users.noreply.github.com>
2021-09-01 22:42:17 +0100

    Update app.py

--- .binder/app.py
+++ .binder/app.py
@@ -1,5 +1,19 @@
-from streamlit_call import *
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import streamlit as st
+
+    
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def download_model(model_name):
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name,use_fast=False)
+    return model, tokenizer
+
+
+
+model_name_fr_ar="Helsinki-NLP/opus-mt-fr-ar"
+model_fr_ar, tokenizer_fr_ar = download_model(model_name_fr_ar)
+model_name_ar_fr="Helsinki-NLP/opus-mt-ar-fr"
+model_ar_fr, tokenizer_ar_fr = download_model(model_name_ar_fr)
 
 st.markdown("<h1 style='text-align: center; color: red;'>Machine Translation using Transformers<br><br></h1>", unsafe_allow_html=True)
 
