389812ff1ec53db76779dcf2ffa3de284d2b4435
HICHAM CHATER <45130784+hichamDRI@users.noreply.github.com>
2021-09-01 21:17:58 +0100

    Update app.py

--- .binder/app.py
+++ .binder/app.py
@@ -15,35 +15,33 @@ model_fr_ar, tokenizer_fr_ar = download_model(model_name_fr_ar)
 model_name_ar_fr="Helsinki-NLP/opus-mt-ar-fr"
 model_ar_fr, tokenizer_ar_fr = download_model(model_name_ar_fr)
 
-st.markdown("<h1 style='text-align: center; color: red;'>Machine Translation using Transformers</h1>", unsafe_allow_html=True)
-st.title('Frensh to Arabic Translator')
+st.markdown("<h1 style='text-align: center; color: red;'>Machine Translation using Transformers<br><br></h1>", unsafe_allow_html=True)
 
-text = st.text_area("Enter Text :", value='', height=None, max_chars=None, key="FrenshText")
-
-if st.button('Translate', key="FrenshBtn"):
-    if text == '':
-        st.write('Please enter Frensh text for translation') 
-    else: 
-        inputs = tokenizer_fr_ar.encode(text, return_tensors="pt")
-        outputs = model_fr_ar.generate(inputs, max_length=128, num_beams=4, early_stopping=True)
-        out=tokenizer_fr_ar.decode(outputs[0])
-        st.text_area('Translation Result :', str(out).strip('<pad>'), key="ArabicResult")
-else: pass
+st.title('Choose your translation option :')
+option = st.selectbox('',('From Frensh To Arabic', 'From Arabic To Frensh'))
 
 
-st.title('Arabic to Frensh Translator')
-text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key="ArabicText")
-
-if st.button('Translate', key="ArabicBtn"):
-    if text == '':
-        st.write('Please enter Arabic text for translation') 
-    else: 
-        inputs = tokenizer_ar_fr.encode(text, return_tensors="pt")
-        outputs = model_ar_fr.generate(inputs, max_length=128, num_beams=4, early_stopping=True)
-        out=tokenizer_ar_fr.decode(outputs[0])
-        st.text_area('Translation Result :', str(out).strip('<pad>'), key="FrenshResult")
-else: pass
-
+text = st.text_area("Enter Text :", value='', max_chars=None, key="FrenshText")
+if option=='From Frensh To Arabic':
+    if st.button('Translate', key="FrenshBtn"):
+        if text == '':
+            st.write('Please enter Frensh text for translation') 
+        else: 
+            inputs = tokenizer_fr_ar.encode(text, return_tensors="pt")
+            outputs = model_fr_ar.generate(inputs, max_length=128, num_beams=4, early_stopping=True)
+            out=tokenizer_fr_ar.decode(outputs[0])
+            st.text_area('Translation Result :', str(out).strip('<pad>'))
+    else: pass
+else:
+    if st.button('Translate', key="ArabicBtn"):
+        if text == '':
+            st.write('Please enter Arabic text for translation') 
+        else: 
+            inputs = tokenizer_ar_fr.encode(text, return_tensors="pt")
+            outputs = model_ar_fr.generate(inputs, max_length=128, num_beams=4, early_stopping=True)
+            out=tokenizer_ar_fr.decode(outputs[0])
+            st.text_area('Translation Result :', str(out).strip('<pad>'))
+    else: pass
 
 
 st.markdown("<h3 style='text-align: center; color: green;'>Created By: CHATER hicham & ABOURACHID Tarik</h3>", unsafe_allow_html=True)
