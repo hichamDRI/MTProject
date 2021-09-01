2466f3525f832ffeeac0b6530a32e4af24149653
HICHAM CHATER <45130784+hichamDRI@users.noreply.github.com>
2021-09-01 21:20:09 +0100

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -12,3 +12,19 @@ def load_jupyter_server_extension(nbapp):
             "--runner.fixMatplotlib=True"
         ]
     )
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import streamlit as st
+
+    
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def download_model(model_name):
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name,use_fast=False)
+    return model, tokenizer
+
+
+
+model_name_fr_ar="Helsinki-NLP/opus-mt-fr-ar"
+model_fr_ar, tokenizer_fr_ar = download_model(model_name_fr_ar)
+model_name_ar_fr="Helsinki-NLP/opus-mt-ar-fr"
+model_ar_fr, tokenizer_ar_fr = download_model(model_name_ar_fr)
